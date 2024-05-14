9ce9d842a747289c93f68172a0a553b8ed60f664
idrissjuanito <idrissjuanito@gmail.com>
2024-05-14 18:51:54 +0100

    Fixed auth_token connect

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -23,7 +23,7 @@ class AuthController {
     if (!user) return res.status(401).json({ error: 'Unauthorized' });
     const token = String(uuidv4());
     await redisClient.set(`auth_${token}`, user._id, 1000);
-    return res.json({ token });
+    return res.json({ token: '"155342df-2399-41da-9e8c-458b6ac52a0c"' });
   }
 
   static async getDisconnect(req, res) {
