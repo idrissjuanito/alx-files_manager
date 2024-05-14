6281a0f050df5034a71335f949ec9fc9e190f9a4
idrissjuanito <idrissjuanito@gmail.com>
2024-05-14 19:12:04 +0100

    Fixed auth_token connect 3

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,6 @@
 import sha1 from 'sha1';
 import { v4 as uuidv4 } from 'uuid';
-import { ObjectId } from 'mongodb';
+// import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
@@ -21,8 +21,8 @@ class AuthController {
       $and: [{ email }, { password: sha1(password) }],
     });
     if (!user) return res.status(401).json({ error: 'Unauthorized' });
-    const token = String(uuidv4());
-    await redisClient.set(`auth_${token}`, user._id, 1000);
+    const token = uuidv4();
+    await redisClient.set(`auth_${token}`, user._id, 86400);
     return res.json({ token });
   }
 
